50d7657d395866f959f07fcbd07fa689abf84870
muk-jjang <whgdk0911@khu.ac.kr>
2024-09-08 22:38:28 +0900

    add: 거리두기확인

--- 7주차/윤성준/거리두기확인.py
+++ 7주차/윤성준/거리두기확인.py
@@ -8,7 +8,42 @@
 
 
 '''
+from collections import deque
 
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def bfs(candidate, place): # candidate는 'P'
+    queue = deque()
+    visited = [[False]*5 for _ in range(5)]
+    queue.append(candidate)
+    cnt = 0
+    
+    while queue:
+        x, y = queue.popleft()
+        visited[x][y] = True
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= 5 or ny < 0 or ny >= 5:
+                continue
+            if visited[nx][ny]:
+                continue
+            if place[nx][ny] == 'X':
+                continue
+            if place[nx][ny] == 'P':
+                return False
+            queue.append((nx, ny))
+        
+        cnt += 1
+        if cnt == 2:
+            return True
+    return True
+            
+
+def solution(places):
+    answers = []
+    
 
 def solution(places):
     answer = []
